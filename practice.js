dd796c3673a7f1b4ba65a255dc92de934ba8ca8a
Patrick McNamee <pjmcnamee@comcast.net>
2019-02-27 15:21:35 -0700

    halfway done

--- practice.js
+++ practice.js
@@ -18,6 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
+  return arr[0]
+}
 
 
 
@@ -33,7 +36,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length-1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -79,7 +92,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder(nums){
+  const arr = []
+  for(let i = 0; i <nums.length; i++){
+    if(nums[i]%2 === 0){
+      arr.push(nums[i])
+    }
+  }
+  return arr
+}
 
 
 
@@ -106,6 +127,21 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+function divider(numbersArray){
+  const evenArr = []
+  const oddArr = []
+  for(let i = 0; i < numbersArray.length; i++){
+    if(numbersArray[i] % 2 === 0){
+      evenArr.push(numbersArray[i])
+    }else {
+      oddArr.push(numbersArray[i])
+    }
+  }
+  const sortedArr = []
+  sortedArr.push(evenArr)
+  sortedArr.push(oddArr)
+  return sortedArr
+}
 
 
 
@@ -127,6 +163,8 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
+
+
 
 
 
